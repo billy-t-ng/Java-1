ede4d9cc2ee8f57e3158ac1599b2f1c97ed8316a
unknown <billytng4@gmail.com>
2024-03-25 15:42:20 -0700

    update comment

--- part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -4,6 +4,5 @@ public class AdaLovelace {
     public static void main(String[] args) {
         // Write your program here
         System.out.println("Ada Lovelace");
-        // testhjklhjlkhlhjjj
     }
 }
