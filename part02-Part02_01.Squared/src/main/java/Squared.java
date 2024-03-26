8c5074b8e82362bf27bd54213df75d23f356cdb0
unknown <billytng4@gmail.com>
2024-03-25 17:33:19 -0700

    Added problem set solution

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Squared {
 
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int input = Integer.valueOf(scanner.nextLine());
+        int squared = input *= input;
+        System.out.println(squared);
 
     }
 }
